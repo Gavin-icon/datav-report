8559893b4c6c2f296bcfea063c8b6ac331c63b90
sam <247765564@qq.com>
2020-05-16 00:20:07 +0800

    销售趋势组件

--- src/components/TotalUsers/index.vue
+++ src/components/TotalUsers/index.vue
@@ -4,7 +4,7 @@
     value="1,087,503"
   >
     <template>
-      <div id="total-users-chart" :style="{ width: '100%', height: '100%' }" />
+      <v-chart :options="getOptions()" />
     </template>
     <template v-slot:footer>
       <div class="total-users-footer">
@@ -24,81 +24,81 @@
 
   export default {
     mixins: [commonCardMixin],
-    mounted() {
-      const chartDom = document.getElementById('total-users-chart')
-      const chart = this.$echarts.init(chartDom)
-      chart.setOption({
-        grid: {
-          left: 0,
-          right: 0,
-          top: 0,
-          bottom: 0
-        },
-        xAxis: {
-          type: 'value',
-          show: false
-        },
-        yAxis: {
-          type: 'category',
-          show: false
-        },
-        series: [{
-          type: 'bar',
-          stack: '总量',
-          data: [100],
-          barWidth: 10,
-          itemStyle: {
-            color: '#45c946'
-          }
-        }, {
-          type: 'bar',
-          stack: '总量',
-          data: [250],
-          itemStyle: {
-            color: '#eee'
-          }
-        }, {
-          type: 'custom',
-          stack: '总量',
-          data: [100],
-          renderItem: (params, api) => {
-            const value = api.value(0)
-            const endPoint = api.coord([value, 0])
-
-            return {
-              type: 'group',
-              position: endPoint,
-              children: [{
-                type: 'path',
-                shape: {
-                  d: 'M1024 255.996 511.971 767.909 0 255.996 1024 255.996z',
-                  x: -5,
-                  y: -20,
-                  width: 10,
-                  height: 10,
-                  layout: 'cover'
-                },
-                style: {
-                  fill: '#45c946'
-                }
-              }, {
-                type: 'path',
-                shape: {
-                  d: 'M0 767.909l512.029-511.913L1024 767.909 0 767.909z',
-                  x: -5,
-                  y: 10,
-                  width: 10,
-                  height: 10,
-                  layout: 'cover'
-                },
-                style: {
-                  fill: '#45c946'
-                }
-              }]
+    methods: {
+      getOptions() {
+        return {
+          grid: {
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0
+          },
+          xAxis: {
+            type: 'value',
+            show: false
+          },
+          yAxis: {
+            type: 'category',
+            show: false
+          },
+          series: [{
+            type: 'bar',
+            stack: '总量',
+            data: [100],
+            barWidth: 10,
+            itemStyle: {
+              color: '#45c946'
             }
-          }
-        }]
-      })
+          }, {
+            type: 'bar',
+            stack: '总量',
+            data: [250],
+            itemStyle: {
+              color: '#eee'
+            }
+          }, {
+            type: 'custom',
+            stack: '总量',
+            data: [100],
+            renderItem: (params, api) => {
+              const value = api.value(0)
+              const endPoint = api.coord([value, 0])
+
+              return {
+                type: 'group',
+                position: endPoint,
+                children: [{
+                  type: 'path',
+                  shape: {
+                    d: 'M1024 255.996 511.971 767.909 0 255.996 1024 255.996z',
+                    x: -5,
+                    y: -20,
+                    width: 10,
+                    height: 10,
+                    layout: 'cover'
+                  },
+                  style: {
+                    fill: '#45c946'
+                  }
+                }, {
+                  type: 'path',
+                  shape: {
+                    d: 'M0 767.909l512.029-511.913L1024 767.909 0 767.909z',
+                    x: -5,
+                    y: 10,
+                    width: 10,
+                    height: 10,
+                    layout: 'cover'
+                  },
+                  style: {
+                    fill: '#45c946'
+                  }
+                }]
+              }
+            }
+          }]
+        }
+      }
     }
   }
 </script>
